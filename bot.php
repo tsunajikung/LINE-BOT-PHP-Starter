46f121a36dad255ef79019d2927ef2c03adde52f
Jirasak Kammakorn <jirasak.ka.54@ubu.ac.th>
2016-12-09 17:02:27 +0700

    fix bot text

--- bot.php
+++ bot.php
@@ -32,8 +32,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				//'text' => $text
-				'text' => 'I AM GROOT !'
+				'text' => $text
+				//'text' => 'I AM GROOT !'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
